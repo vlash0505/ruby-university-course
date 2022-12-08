caa71ad960d9e287d34943ab8ec340b8d3b5cb87
vlash0505 <shupliakovvl@gmail.com>
2022-12-08 03:51:55 +0200

    [Lab7] refactor

--- Lab7/students/config/routes.rb
+++ Lab7/students/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :students
   #get 'home/index'
-  root 'home#index'
+  root 'students#index'
 
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
